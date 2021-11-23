vrothberg/moby

commit 317e94be44d64cf0d9e7ef4fc20818d1d3680495

evaluate symlinks before relabeling mount source
Signed-off-by: Antonio Murdaca <runcom@redhat.com>

volume/volume.go
@@ -128,7 +128,17 @@ func (m *MountPoint) Setup(mountLabel string, rootUID, rootGID int) (path string
 	defer func() {
 		if err == nil {
 			if label.RelabelNeeded(m.Mode) {
-				if err = label.Relabel(m.Source, mountLabel, label.IsShared(m.Mode)); err != nil {
+				sourcePath, err := filepath.EvalSymlinks(m.Source)
+				if err != nil {
+					path = ""
+					err = errors.Wrapf(err, "error evaluating symlink from mount source '%s'", m.Source)
+					return
+				}
+				err = label.Relabel(sourcePath, mountLabel, label.IsShared(m.Mode))
+				if err == syscall.ENOTSUP {
+					err = nil
+				}
+				if err != nil {
 					path = ""
 					err = errors.Wrapf(err, "error setting label on mount source '%s'", m.Source)
 					return

daemon/volumes.go
@@ -152,11 +152,6 @@ func (daemon *Daemon) registerMountPoints(container *container.Container, hostCo
 			if bind.Driver == volume.DefaultDriverName {
 				setBindModeIfNull(bind)
 			}
-			if label.RelabelNeeded(bind.Mode) {
-				if err := label.Relabel(bind.Source, container.MountLabel, label.IsShared(bind.Mode)); err != nil {
-					return err
-				}
-			}
 		}
 
 		binds[bind.Destination] = true

scantist-ossops-m2/docker

commit 704978239dc81b1f15572f4735f3afac8b90e690

Fix masking/ignoring relabeling error
Commit 317e94be44d64cf0 introduced a local variable err, which masked
the err used as a named return value. As a result, any relabeling error
(happened in that defer) is not returned, and the caller gets "", nil,
moving on to performing a bind mount with empty m.Source argument
(which, apparently, results in mounting a current directory).

This can be reproduced by trying to add a bind mount which has no space
left (see RHBZ link below for details).

This issue was spotted upstream during the review, and fixed before the
merge (see [1]). In this repository, though, it was never fixed.

Fixes: https://bugzilla.redhat.com/1878621

[1]  https://github.com/moby/moby/pull/34792#issuecomment-330355030

Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

volume/volume.go
@@ -140,7 +140,8 @@ func (m *MountPoint) Setup(mountLabel string, rootUID, rootGID int, checkFun fun
 	defer func() {
 		if err == nil {
 			if label.RelabelNeeded(m.Mode) {
-				sourcePath, err := filepath.EvalSymlinks(m.Source)
+				var sourcePath string
+				sourcePath, err = filepath.EvalSymlinks(m.Source)
 				if err != nil {
 					path = ""
 					err = errors.Wrapf(err, "error evaluating symlink from mount source '%s'", m.Source)